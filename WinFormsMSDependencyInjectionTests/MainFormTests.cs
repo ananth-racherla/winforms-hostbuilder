ce405dfd100a72a7bea55c74ee8ca5e36a9286e7
Ananth Racherla <ananth.racherla@promethean.tv>
2021-05-17 19:04:08 -0700

    Add UI test project

---
+++ WinFormsMSDependencyInjectionTests/MainFormTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace WinFormsMSDependencyInjection.Tests
+{
+    [TestClass()]
+    public class MainFormTests
+    {
+        [TestMethod()]
+        public void MainFormTest()
+        {
+            var thingMoq = new Mock<ILogger<ThingTwo>>();
+            var relayMoq = new Mock<IOptions<RelayConfig>>();
+            relayMoq.Setup(s => s.Value).Returns(new RelayConfig { Position = "Test", Type = "MyType" });
+
+            var thingTwo = new ThingTwo(thingMoq.Object, relayMoq.Object);
+            Assert.IsNotNull(thingTwo.Hello());
+        }
+    }
+}
